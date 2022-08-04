c117607691033e091b9129b35f6afe46d0201430
Maximiano Eduardo <edu.maxi.96@gmail.com>
2022-08-04 10:32:51 -0300

    :sparkles:  Criando Eventos que definirão a logica
    (obs : late counter ?)

--- lib/counter_bloc.dart
+++ lib/counter_bloc.dart
@@ -1,6 +1,11 @@
 import 'dart:async';
 
+// Definindo eventos que são recebidos em sink e passados para stream
+enum CounterAction { increment, decrement, reset }
+
 class CounterBloc {
+  late int counter;
+
   // Variavel Privada sendo assim tendo acesso apenas a entrada e saida de dados
   final _stateStreamController = StreamController<int>();
 
@@ -8,4 +13,26 @@ class CounterBloc {
   StreamSink<int> get counterSink => _stateStreamController.sink;
   // Saida de Dados = stream
   Stream<int> get counterStream => _stateStreamController.stream;
+
+  // Variavel Privada sendo assim tendo acesso apenas a entrada e saida de dados
+  final _eventStreamController = StreamController<CounterAction>();
+  // Recebendo enum de possiveis eventos
+
+  // Entrada do Evento = Sink
+  StreamSink<CounterAction> get eventSink => _eventStreamController.sink;
+  // Saida do Evento = stream
+  Stream<CounterAction> get eventStream => _eventStreamController.stream;
+
+  CounterBloc() {
+    counter = 0;
+    eventStream.listen((event) {
+      if (event == CounterAction.increment) {
+        counter++;
+      } else if (event == CounterAction.decrement) {
+        counter--;
+      } else if (event == CounterAction.reset) {
+        counter = 0;
+      }
+    });
+  }
 }
