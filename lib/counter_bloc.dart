29d0ded77de349e3c70b1e5c6a63aaa279125804
Maximiano Eduardo <edu.maxi.96@gmail.com>
2022-08-04 10:07:59 -0300

    :sparkles: Criando logica de Dados

--- lib/counter_bloc.dart
+++ lib/counter_bloc.dart
@@ -1,1 +1,11 @@
-class CounterBloc {}
+import 'dart:async';
+
+class CounterBloc {
+  // Variavel Privada sendo assim tendo acesso apenas a entrada e saida de dados
+  final _stateStreamController = StreamController<int>();
+
+  // Entrada de Dados = Sink
+  StreamSink<int> get counterSink => _stateStreamController.sink;
+  // Saida de Dados = stream
+  Stream<int> get counterStream => _stateStreamController.stream;
+}
